4665df9227638916784d67482177787711458773
Rychardson <rychard.ribeiro.souza@hotmail.com>
2023-01-04 20:10:30 -0300

    adicionando o novo projeto

--- README.md
+++
@@ -1,4 +0,0 @@
-# Desafio-Include---app-mobile
-Este repositório irá conter os códigos desenvolvidos no processo trainee da include para um app mobile
-
-
--- ambiente-react/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
-
-ReactDOM.render(
-    <h1><div>Teste</div></h1>,
-    document.getElementById('root')
-)
